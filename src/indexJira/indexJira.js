1005a5844fa77aa67ca3adb43633a1fc8561ed33
dndkute01 <dndkute01@gmail.com>
2023-02-03 20:31:07 +0700

    create task

--- src/indexJira/indexJira.js
+++ src/indexJira/indexJira.js
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
 import { GET_PROJECT_DETAIL_API } from "../redux/constant/jiraConstant";
-import HeaderMainJira from "./../Pages/Home/HeaderMainJira";
-import InforMainJira from "./../Pages/Home/InforMainJira";
-import ContentMainJira from "./../Pages/Home/ContentMainJira";
+import HeaderMainJira from "../Pages/Home/HeaderMainJira";
+import InforMainJira from "../Pages/Home/InforMainJira";
+import ContentMainJira from "../Pages/Home/ContentMainJira";
 
 export default function IndexJira(props) {
   const { projectDetail } = useSelector((state) => state.ProjectReducer);
